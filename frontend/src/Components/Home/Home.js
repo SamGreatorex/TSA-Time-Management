468fd37ad929075400ddad922b748e0e98b51e11
SamGreat <samantha@greatorex.org.uk>
2023-06-26 16:39:29 +0100

    Updated adding new task with multi select

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,92 +1,110 @@
-import React, {useEffect, useState} from 'react';
-import {Select, Row, Col, Space, Tooltip, Button,Table, Input, Modal, Form} from 'antd';
-import * as tcActions from '../../redux/actions/timecards';
-import { connect} from 'react-redux';
-import {bindActionCreators } from 'redux';
-import moment, { parseZone } from 'moment';
-import { v4 as uuid } from 'uuid';
-import Sider from 'antd/es/layout/Sider';
+import React, { useEffect, useState } from "react";
+import {
+  Select,
+  Row,
+  Col,
+  Space,
+  Tooltip,
+  Button,
+  Table,
+  Input,
+  Modal,
+  Form,
+} from "antd";
+import * as tcActions from "../../redux/actions/timecards";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import moment, { parseZone } from "moment";
+import { v4 as uuid } from "uuid";
+import Sider from "antd/es/layout/Sider";
 
-function Home({actions, timecards, tasks}) {
-  const initSummary = {weekTimeLogged: "0", dailyTimeLogged: "0"}
+function Home({ actions, timecards, tasks }) {
+  const initSummary = { weekTimeLogged: "0", dailyTimeLogged: "0" };
   const [summaryData, setSummaryData] = useState(initSummary);
 
-  
   useEffect(() => {
-    if(timecards.length === 0) actions.getUserTimecards('samg');
+    if (timecards.length === 0) actions.getUserTimecards("samg");
   }, []);
 
   useEffect(() => {
-    console.log('timecards updated', timecards);
     let totalDailyHours = 0;
 
-    if(timecards?.length > 0){
-      const cTimeCard = timecards.find(x=>moment(x.StartDate).toString() === moment().startOf('isoWeek').toString());
-      const dailyTasks = cTimeCard?.Tasks?.filter(x=>(moment(x.StartTime).startOf('day').toString() === moment().startOf('day').toString()));
-      console.log('Daily Tasks', dailyTasks);
-    
-      let totalWeeklyDuration = cTimeCard?.Tasks?.map(item => item.totalDuration).reduce((prev, next) => prev + next);
-      let weekHrs = Math.floor(totalWeeklyDuration / 60);
-      let weekMns = totalWeeklyDuration - (weekHrs * 60);
-      let totalWeeklyHours = `${weekHrs || 0}hrs ${weekMns || 0}min`
+    if (timecards?.length > 0) {
+      const cTimeCard = timecards.find(
+        (x) =>
+          moment(x.StartDate).toString() ===
+          moment().startOf("isoWeek").toString()
+      );
+      const dailyTasks = cTimeCard?.Tasks?.filter(
+        (x) =>
+          moment(x.StartTime).startOf("day").toString() ===
+          moment().startOf("day").toString()
+      );
 
-      if(dailyTasks && dailyTasks.length > 0)
-      {
-      let totalDailyDuration = dailyTasks.map(item => item.totalDuration).reduce((prev, next) => prev + next);
-      let dilyHrs = Math.floor(totalDailyDuration / 60);
-      let dailyMns = totalDailyDuration - (dilyHrs * 60);
-      totalDailyHours = `${dilyHrs}hrs ${dailyMns}min`
+      let totalWeeklyDuration = cTimeCard?.Tasks?.map(
+        (item) => item.totalDuration
+      ).reduce((prev, next) => prev + next);
+      let weekHrs = Math.floor(totalWeeklyDuration / 60);
+      let weekMns = totalWeeklyDuration - weekHrs * 60;
+      let totalWeeklyHours = `${weekHrs || 0}hrs ${weekMns || 0}min`;
+
+      if (dailyTasks && dailyTasks.length > 0) {
+        let totalDailyDuration = dailyTasks
+          .map((item) => item.totalDuration)
+          .reduce((prev, next) => prev + next);
+        let dilyHrs = Math.floor(totalDailyDuration / 60);
+        let dailyMns = totalDailyDuration - dilyHrs * 60;
+        totalDailyHours = `${dilyHrs}hrs ${dailyMns}min`;
       }
-      let updatedData = {...initSummary};
+      let updatedData = { ...initSummary };
       updatedData.weekTimeLogged = totalWeeklyHours;
       updatedData.dailyTimeLogged = totalDailyHours;
       setSummaryData(updatedData);
     }
-   
   }, [timecards]);
 
   return (
     <div>
       <Row>
-      <h2>Weekly Summary</h2>
+        <h2>Weekly Summary</h2>
       </Row>
       <Row>
         <Col>Week Start Date: </Col>
-        <Col>{moment().startOf('isoWeek').format('dddd  Do MMMM')}</Col>
+        <Col>{moment().startOf("isoWeek").format("dddd  Do MMMM")}</Col>
       </Row>
       <Row>
         <Col>Week Time logged: </Col>
         <Col>{summaryData.weekTimeLogged}</Col>
       </Row>
       <Row>
-      <h2>Daily Summary</h2>
+        <h2>Daily Summary</h2>
       </Row>
       <Row>
         <Col>Date: </Col>
-        <Col>{moment().format('dddd  Do MMMM')}</Col>
+        <Col>{moment().format("dddd  Do MMMM")}</Col>
       </Row>
       <Row>
         <Col>Time Logged: </Col>
         <Col>{summaryData.dailyTimeLogged}</Col>
       </Row>
-      </div>
-  ) 
+    </div>
+  );
 }
 function mapStateToProps(state) {
   return {
-  timecards: state.timecards.usercards
+    timecards: state.timecards.usercards,
   };
 }
-
 
 function mapDispatchToProps(dispatch) {
   return {
     actions: {
-      getUserTimecards: bindActionCreators(tcActions.getUserTimecards, dispatch)
-    }
+      getUserTimecards: bindActionCreators(
+        tcActions.getUserTimecards,
+        dispatch
+      ),
+    },
   };
 }
-
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- frontend/src/Components/Timecards/Timecards.js
+++ frontend/src/Components/Timecards/Timecards.js
@@ -1,310 +1,338 @@
-import React, {useEffect, useState} from 'react';
-import {Select, Row, Col, Space, Tooltip, Button,Table, Input, Modal, Form} from 'antd';
-import * as tcActions from '../../redux/actions/timecards';
-import { connect} from 'react-redux';
-import {bindActionCreators } from 'redux';
-import moment, { parseZone } from 'moment';
-import { v4 as uuid } from 'uuid';
-
+import React, { useEffect, useState } from "react";
+import {
+  Select,
+  Row,
+  Col,
+  Tooltip,
+  Button,
+  Table,
+  Input,
+  Modal,
+  Form,
+  Cascader,
+} from "antd";
+import * as tcActions from "../../redux/actions/timecards";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import moment, { parseZone } from "moment";
+import { v4 as uuid } from "uuid";
 
 const { TextArea } = Input;
 const { Option } = Select;
 
- function Timecards({actions, timecards, tasks}) {
-
+function Timecards({ actions, timecards, tasks }) {
   const [form] = Form.useForm();
-  const [lTimecards, setlTimecards] = useState([])
-  const [currentTimecard, setCurrentTimecard] = useState(null)
-  const [updatingTask, setUpdatingTask] = useState(false) 
-  const [tcDescription, setTCDescription] = useState("") 
-  const [isTaskModalOpen, setIsTaskModalOpen] = useState(false)
+  const [lTimecards, setlTimecards] = useState([]);
+  const [currentTimecard, setCurrentTimecard] = useState(null);
+  const [updatingTask, setUpdatingTask] = useState(false);
+  const [tcDescription, setTCDescription] = useState("");
+  const [taskSelectOptions, setTaskSelectOptions] = useState([]);
 
   useEffect(() => {
-    actions.getUserTimecards('samg');
+    actions.getUserTimecards("samg");
     actions.getTasks();
   }, []);
 
   useEffect(() => {
-    console.log('timecards changed', timecards);
-      setlTimecards(timecards);
-      const cTimeCard = timecards.find(x=>moment(x.StartDate).toString() === moment().startOf('isoWeek').toString());
-      console.log('Current Time Card Set as:', cTimeCard);
-      setCurrentTimecard(cTimeCard);
+    setlTimecards(timecards);
+    const cTimeCard = timecards.find(
+      (x) =>
+        moment(x.StartDate).toString() ===
+        moment().startOf("isoWeek").toString()
+    );
+    setCurrentTimecard(cTimeCard);
+    configureTaskSelectOptions();
   }, [timecards]);
 
   const timeCardsTasks = [
     {
-        key: 'taskId',
-        dataIndex: 'taskId',
-        hidden: true
-      },
-      {
-        title: 'Task Name',
-        key: 'Name',
-        dataIndex: 'Name',
-        align: 'center',
-      },
-      {
-        title: 'Type',
-        key: 'Type',
-        dataIndex: 'Type',
-        align: 'center',
-      },
-      {
-        key: 'actions',
-        align: 'center',
-        render:  (record) => {
-           let tc =  currentTimecard.Tasks?.find(x=>x.TaskTypeId === record.TaskId && (moment(x.StartTime).startOf('day').toString() === moment().startOf('day').toString())) || null;
-           const RecordUpdating = currentTimecard.Tasks?.find(x=>x.IsInProgress && x.IsInProgress === true);
-            return (
-              <div>
-                {/* Only Show start button if no other task is in progress*/}
-              {!RecordUpdating && 
-                <Tooltip title="Start Task">
-                   <Button onClick={() =>OnStartTask(record)} type="primary">Start </Button>
-                </Tooltip>
-              }
+      key: "taskId",
+      dataIndex: "taskId",
+      hidden: true,
+    },
+    {
+      title: "Task Name",
+      key: "Name",
+      dataIndex: "Name",
+      align: "center",
+    },
+    {
+      title: "Type",
+      key: "Type",
+      dataIndex: "Type",
+      align: "center",
+    },
+    {
+      key: "actions",
+      align: "center",
+      render: (record) => {
+        let tc =
+          currentTimecard.Tasks?.find(
+            (x) =>
+              x.TaskTypeId === record.TaskId &&
+              moment(x.StartTime).startOf("day").toString() ===
+                moment().startOf("day").toString()
+          ) || null;
+        const RecordUpdating = currentTimecard.Tasks?.find(
+          (x) => x.IsInProgress && x.IsInProgress === true
+        );
+        return (
+          <div>
+            {/* Only Show start button if no other task is in progress*/}
+            {!RecordUpdating && (
+              <Tooltip title="Start Task">
+                <Button onClick={() => OnStartTask(record)} type="primary">
+                  Start{" "}
+                </Button>
+              </Tooltip>
+            )}
 
-              {RecordUpdating && tc?.IsInProgress === true &&
-                    <Row>
-                      <Col span={20}>
-                        <TextArea rows={2} onChange={(e) => setTCDescription(e.target.value)}/>
-                        </Col>
-                        <Col  span={4}>
-                        <Button type="primary" onClick={() => OnEndTask(record)}>Submit</Button>
-                        </Col>
-                    </Row>
-                 
-                }
-           </div>
-            );
-          },
-      }
+            {RecordUpdating && tc?.IsInProgress === true && (
+              <Row>
+                <Col span={20}>
+                  <TextArea
+                    rows={2}
+                    onChange={(e) => setTCDescription(e.target.value)}
+                  />
+                </Col>
+                <Col span={4}>
+                  <Button type="primary" onClick={() => OnEndTask(record)}>
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+            )}
+          </div>
+        );
+      },
+    },
   ];
 
-
+  const configureTaskSelectOptions = async () => {
+    const options = [];
+    if (tasks?.length === 0) return;
+    let taskSorted = tasks.sort((a, b) =>
+      a.Name.toLowerCase() > b.Name.toLowerCase() ? 1 : -1
+    );
+    for (let i = 0; i < taskSorted.length; i++) {
+      let d = taskSorted[i];
+      options.find((x) => x.value === d.Type)
+        ? options
+            .find((x) => x.value === d.Type)
+            .children.push({ value: d.TaskId, label: d.Name })
+        : options.push({
+            value: d.Type,
+            label: d.Type,
+            children: [{ value: d.TaskId, label: d.Name }],
+          });
+    }
+    setTaskSelectOptions(options);
+  };
 
   const OnStartTask = async (record) => {
-
     let existingTask = await getExistingTask(record.TaskId);
 
     //check if task already exists
     let newTask = {
-        StartTime: existingTask?.StartTime ? existingTask.StartTime : moment().toISOString(),
-        totalDuration: existingTask?.totalDuration ? existingTask.totalDuration : 0,
-        TaskId: existingTask?.TaskId ? existingTask.TaskId :  uuid(),
-        TaskTypeId: record.TaskId,
-        Notes:  existingTask?.Notes ? existingTask.Notes : [],
-        IsInProgress : true,
-        TaskStartTime: moment().toISOString()
+      StartTime: existingTask?.StartTime
+        ? existingTask.StartTime
+        : moment().toISOString(),
+      totalDuration: existingTask?.totalDuration
+        ? existingTask.totalDuration
+        : 0,
+      TaskId: existingTask?.TaskId ? existingTask.TaskId : uuid(),
+      TaskTypeId: record.TaskId,
+      Notes: existingTask?.Notes ? existingTask.Notes : [],
+      IsInProgress: true,
+      TaskStartTime: moment().toISOString(),
     };
 
     onUpdateTask(newTask);
-    setUpdatingTask(true)
-  }
+    setUpdatingTask(true);
+  };
 
   const getExistingTask = async (taskId) => {
-    let existingTask = {...currentTimecard.Tasks?.find(x=>x.TaskTypeId === taskId && (moment(x.StartTime).startOf('day').toString() === moment().startOf('day').toString()))};
+    let existingTask = {
+      ...currentTimecard.Tasks?.find(
+        (x) =>
+          x.TaskTypeId === taskId &&
+          moment(x.StartTime).startOf("day").toString() ===
+            moment().startOf("day").toString()
+      ),
+    };
     return existingTask;
-  }
+  };
 
   const OnEndTask = async (record) => {
+    let task = { ...currentTimecard.Tasks.find((x) => x.IsInProgress) };
+    let notes = [...task.Notes];
 
-    let task = {...currentTimecard.Tasks.find(x=>x.IsInProgress)};
-     let notes = [...task.Notes];
+    let startDate = await formatDate(moment(task.TaskStartTime));
+    let endDate = await formatDate(moment());
+    var duration = moment.duration(moment(endDate).diff(startDate));
+    var minutes = duration.asMinutes() === 0 ? 15 : duration.asMinutes();
 
+    notes.push({
+      noteId: uuid(),
+      StartTime: task.TaskStartTime,
+      duration: minutes,
+      note: tcDescription,
+    });
 
-     let startDate = await formatDate(moment(task.TaskStartTime));
-     let endDate = await formatDate(moment());
-      var duration = moment.duration(moment(endDate).diff(startDate));
-     var minutes = duration.asMinutes() === 0 ? 15 : duration.asMinutes();
-
-
-     notes.push({noteId: uuid(), StartTime: task.TaskStartTime, duration: minutes, note: tcDescription});
-
-
-    console.log('!!!', task, notes);
     delete task.TaskStartTime;
     task.totalDuration = parseInt(minutes) + parseInt(task.totalDuration);
-    task.Notes =  notes;
+    task.Notes = notes;
     task.IsInProgress = false;
-    await onUpdateTask(task)
+    await onUpdateTask(task);
     setUpdatingTask(false);
-   }
+  };
 
   const OnTimeCardChanged = async (record) => {
-    console.log('Timecard changed', record);
-    setCurrentTimecard(lTimecards.find(x=>x.TimeCardId === record));
-  }
+    setCurrentTimecard(lTimecards.find((x) => x.TimeCardId === record));
+  };
 
-
-  const onCreateTask = async (task) => {
-    console.log('Adding task', task);
-    
-   // let existingTasks = currentTimecard.AvailableTasks;
-   // existingTasks.push(tasks.find(x=> x.TaskId === task.type));
+  const OnAddTask = async (task) => {
     let updatedTasks = [...currentTimecard.AvailableTasks];
-    updatedTasks.push(tasks.find(x=> x.TaskId === task.type));
-    let updatedTimecard = {...currentTimecard}
-    updatedTimecard.AvailableTasks = updatedTasks
-    console.log('Current timecard is now', updatedTimecard);
-    actions.updateTimecard(updatedTimecard);
-    setIsTaskModalOpen(false);
-  }
+    if (!updatedTasks.find((x) => x.TaskId === task[1])) {
+      updatedTasks.push(tasks.find((x) => x.TaskId === task[1]));
+      let updatedTimecard = { ...currentTimecard };
+      updatedTimecard.AvailableTasks = updatedTasks;
+      actions.updateTimecard(updatedTimecard);
+    }
+  };
 
   const onUpdateTask = async (task) => {
-
-     let allTasks = [...currentTimecard.Tasks.filter(x=> x.TaskId !== task.TaskId)] 
-     allTasks.push(task);
-     let updatedTimeCard = {...currentTimecard};
-     updatedTimeCard.Tasks = allTasks;
-     console.log('!!!', updatedTimeCard);
-    // let existingTask = await getExistingTask(task.TaskId);
-    // console.log('Existing Task Found', existingTask)
-    // let updatedTasks = existingTask
-    // ? [...currentTimecard.Tasks.filter(x=> !(x.TaskId === existingTask.TaskId && x.StartTime === existingTask.StartTime))] 
-    // : [...currentTimecard.Tasks];
-   // updatedTasks = [...updatedTasks, task];  
-
-   //  let updatedTimeCard = {...currentTimecard};
-   //  updatedTimeCard.Tasks = updatedTasks;
-   
+    let allTasks = [
+      ...currentTimecard.Tasks.filter((x) => x.TaskId !== task.TaskId),
+    ];
+    allTasks.push(task);
+    let updatedTimeCard = { ...currentTimecard };
+    updatedTimeCard.Tasks = allTasks;
     actions.updateTimecard(updatedTimeCard);
-  }
+  };
 
   const onCreateWeeksCard = async () => {
-  
-      let timecard = {
-        UserId: "samg",
-        TimeCardId: uuid(),
-        AvailableTasks: tasks.filter(x=>x.Default === true),
-        StartDate: moment().startOf('isoWeek').toString(),
-        EndDate: moment().endOf('isoWeek').toString(),
-        Tasks:[]
-      };
-      console.log('Creating Timecard:', timecard);
-      actions.updateTimecard(timecard);
+    let timecard = {
+      UserId: "samg",
+      TimeCardId: uuid(),
+      AvailableTasks: tasks.filter((x) => x.Default === true),
+      StartDate: moment().startOf("isoWeek").toString(),
+      EndDate: moment().endOf("isoWeek").toString(),
+      Tasks: [],
+    };
+    actions.updateTimecard(timecard);
+  };
 
-
-  }
-  
-    return (
-        <div>
-          <Row>
-          <h2>Timecards</h2>
-          </Row>
-          <Row>
-          <Col>
-              Start Week: 
-            </Col>
-            <Col>
-              <Select disabled={timecards?.length === 0} style={{ width: '200px' }} onChange={OnTimeCardChanged} defaultValue={()=>  moment().startOf('isoWeek').format('Do MMM YY')}>
-                  {lTimecards.map((tc) => (
-                    <Option key={tc.TimeCardId}>{moment(tc.StartDate).format('Do MMM YY')}</Option>
-                  ))}
-                </Select>
-            </Col>
-            <Col>
-            {!timecards?.find(x=>moment(x.StartDate).toString() === moment().startOf('isoWeek').toString()) && 
+  return (
+    <div>
+      <Row>
+        <h2>Timecards</h2>
+      </Row>
+      <Row>
+        <Col>Start Week:</Col>
+        <Col>
+          <Select
+            disabled={timecards?.length === 0}
+            style={{ width: "200px", paddingLeft: "10px" }}
+            onChange={OnTimeCardChanged}
+            defaultValue={() => moment().startOf("isoWeek").format("Do MMM YY")}
+          >
+            {lTimecards.map((tc) => (
+              <Option key={tc.TimeCardId}>
+                {moment(tc.StartDate).format("Do MMM YY")}
+              </Option>
+            ))}
+          </Select>
+        </Col>
+        <Col>
+          {!timecards?.find(
+            (x) =>
+              moment(x.StartDate).toString() ===
+              moment().startOf("isoWeek").toString()
+          ) && (
             <Tooltip title="create-week-card">
-                     <Button type="primary" onClick={() => onCreateWeeksCard()}> Create Weeks Card </Button>
+              <Button type="primary" onClick={() => onCreateWeeksCard()}>
+                {" "}
+                Create Weeks Card{" "}
+              </Button>
             </Tooltip>
+          )}
+        </Col>
+        <Col>
+          {currentTimecard && (
+            <Cascader
+              style={{ paddingLeft: "10px" }}
+              options={taskSelectOptions || []}
+              onChange={OnAddTask}
+              placeholder="Add New Task"
+            />
+          )}
+        </Col>
+      </Row>
+      <Row>
+        {currentTimecard && (
+          <Table
+            style={{ marginTop: "4px", width: "100%" }}
+            size="small"
+            dataSource={
+              !currentTimecard.AvailableTasks
+                ? []
+                : currentTimecard.AvailableTasks.sort((a, b) =>
+                    a.Type.toLowerCase() > b.Type.toLowerCase() ? 1 : -1
+                  )
             }
-            </Col>
-            <Col>
-            {currentTimecard &&
-            <Tooltip title="create-task">
-                    <Button type="primary" onClick={() => setIsTaskModalOpen(true)}> Add New Task </Button>
-            </Tooltip>
-            }  
-            
-            </Col>
-          </Row>
-          <Row>
-          {currentTimecard && 
-           <Table
-                style={{ marginTop: '4px', width: '100%' }}
-                size='small'
-                dataSource={!currentTimecard.AvailableTasks ? [] : currentTimecard.AvailableTasks.sort((a, b) => a.Type.toLowerCase() > b.Type.toLowerCase() ? 1 : -1)}
-                rowKey={(record) => record.TaskId}
-                columns={timeCardsTasks}
-                pagination={{  
-                  total: currentTimecard.AvailableTasks ? currentTimecard.AvailableTasks.length : 0,
-                  pageSize: 15,
-                  hideOnSinglePage: true,}}
-              /> 
-            }
-          </Row>
-
-      <Modal title="Add Task" open={isTaskModalOpen}  footer={null} onCancel={()=> setIsTaskModalOpen(false)}>
-     
-      <Form name="createTask" labelCol={{ span: 8, }} wrapperCol={{ span: 16, }} form={form} onFinish={onCreateTask}>
-      <Form.Item label="Task Type" name="type" rules={[{required: true, message: 'Please select a type!'}]}>
-      <Select style={{ width: '300px' }}>
-          {tasks?.filter(x=>x.IsVisible).sort((a, b) => a.Type.toLowerCase() > b.Type.toLowerCase() ? 1 : -1).map((tc) => (
-            <Option key={tc.TaskId}>{tc.Type} - {tc.Name}</Option>
-          ))}
-        </Select>
-      </Form.Item>
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Select
-        </Button>
-      </Form.Item>
-    </Form>
-      </Modal>
-
-        </div>
-
-
-    );
+            rowKey={(record) => record.TaskId}
+            columns={timeCardsTasks}
+            pagination={{
+              total: currentTimecard.AvailableTasks
+                ? currentTimecard.AvailableTasks.length
+                : 0,
+              pageSize: 15,
+              hideOnSinglePage: true,
+            }}
+          />
+        )}
+      </Row>
+    </div>
+  );
 }
 
-
-
 async function formatDate(date) {
+  let minutes = date.minutes();
+  let hour = date.hour();
+  var day = date.date();
+  var month = date.month();
+  var year = date.year();
+  if (minutes < 15) minutes = 15;
+  if (minutes > 15 && minutes < 30) minutes = 30;
+  if (minutes > 30 && minutes < 45) minutes = 45;
+  if (minutes > 45) {
+    minutes = 0;
+    hour = hour + 1;
+  }
 
-let minutes = date.minutes();
-let hour = date.hour();
-var day = date.date();
-var month = date.month();
-var year = date.year();
-if(minutes < 15) minutes = 15;
-if(minutes > 15 && minutes < 30) minutes = 30;
-if(minutes > 30 && minutes < 45) minutes = 45;
-if(minutes > 45) {
-minutes = 0;
-hour = hour +1;
-} 
-
-var newDate = new Date(year, month, day, hour, minutes);
-return moment(newDate).toString();
+  var newDate = new Date(year, month, day, hour, minutes);
+  return moment(newDate).toString();
 }
 
 function mapStateToProps(state) {
   return {
-
-  timecards: state.timecards.usercards,
-   tasks: state.timecards.tasks
+    timecards: state.timecards.usercards,
+    tasks: state.timecards.tasks,
   };
 }
-
 
 function mapDispatchToProps(dispatch) {
   return {
     actions: {
-      getUserTimecards: bindActionCreators(tcActions.getUserTimecards, dispatch),
+      getUserTimecards: bindActionCreators(
+        tcActions.getUserTimecards,
+        dispatch
+      ),
       updateTimecard: bindActionCreators(tcActions.updateTimecard, dispatch),
-      getTasks: bindActionCreators(tcActions.getTasks, dispatch)
-    }
+      getTasks: bindActionCreators(tcActions.getTasks, dispatch),
+    },
   };
 }
-
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Timecards);
